ed9b251035989c047edd6e80a8365b6913781185
Alex McCullough <alexmccullough@outlook.com>
2024-11-24 12:32:19 +0000

    feat: basic setup and navbar added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StarsComponent } from './stars/stars.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'stars',
+    component: StarsComponent,
+  },
+];

59f77f97c9b4ebe7586ed5165caac1a7048eef26
Alex McCullough <alexmccullough@outlook.com>
2024-11-24 13:09:20 +0000

    wip: stars endpoint to be worked on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent],
+  providers: [ApiService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'y4_com661_cw2';
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'stars',
   standalone: true,
   imports: [RouterOutlet],
+  providers: [ApiService],
   templateUrl: './stars.component.html',
 })
-export class StarsComponent {}
+export class StarsComponent {
+  stars_list: any;
+
+  constructor(public apiService: ApiService) {}
+
+  ngOnInit() {
+    this.stars_list = this.apiService.getAllStars();
+  }
+}

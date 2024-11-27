230082767e240e788c63653a2c10b91989be7ec3
Alex McCullough <alexmccullough@outlook.com>
2024-11-27 11:57:15 +0000

    feat: progress on pagination

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -8,15 +8,35 @@ import { WebService } from '../web.service';
   imports: [RouterOutlet],
   providers: [WebService],
   templateUrl: './stars.component.html',
+  styleUrl: './stars.component.css',
 })
 export class StarsComponent {
   stars_list: any;
+  page: number = 1;
 
   constructor(public webService: WebService) {}
 
   ngOnInit() {
-    this.webService.getStars(10).subscribe((response) => {
+    this.webService.getStars(1).subscribe((response) => {
       this.stars_list = response;
     });
   }
+
+  previousPage() {
+    if (this.page > 1) {
+      this.page = this.page - 1;
+      this.webService.getStars(this.page).subscribe((response: any) => {
+        this.stars_list = response;
+      });
+    }
+  }
+
+  // nextPage() {
+  //   if (this.page < this.webService.getLastPageNumber()) {
+  //     this.page = this.page + 1;
+  //     this.webService.getStars(this.page).subscribe((response: any) => {
+  //       this.stars_list = response;
+  //     })
+  //   }
+  // }
 }
--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -1,18 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { get } from 'node:http';
 
 @Injectable()
 export class WebService {
-  pageSize: number = 10;
+  pageSize: number = 16;
+  pageNumber: number = 1;
 
   constructor(private http: HttpClient) {}
 
-  getStars(page: number) {
+  getStars(pageNumber?: number) {
     return this.http.get<any>(
       'http://localhost:5000/api/v1.0/bodies?pn=' +
-        page +
+        pageNumber +
         '&ps=' +
         this.pageSize
     );
   }
+
+  getLastPageNumber() {}
 }

5946938d46bc17e71eb43b2879e557a975657de7
Alex McCullough <alexmccullough@outlook.com>
2024-11-28 12:30:25 +0000

    feat: improved handling of sessionStorage

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,34 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { WebService } from '../web.service';
-import { CommonModule, DecimalPipe } from '@angular/common';
+import { CommonModule, DecimalPipe, isPlatformBrowser } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'stars',
   standalone: true,
-  imports: [CommonModule, DecimalPipe],
+  imports: [RouterOutlet, CommonModule, DecimalPipe],
   providers: [WebService],
   templateUrl: './stars.component.html',
   styleUrl: './stars.component.css',
 })
+
 export class StarsComponent {
   stars_list: any;
   page: number = 1;
   lastPageNumber: number = 0;
 
-  constructor(public webService: WebService) {}
+  constructor(public webService: WebService, @Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      if (sessionStorage['page']) {
+        this.page = Number(sessionStorage['page']);
+      }
+    }
     this.webService.getStars(1).subscribe((response) => {
       this.stars_list = response;
     });
     this.webService.getLastPageNumber().subscribe((response: any) => {
       this.lastPageNumber = response;
-    })
+    });
   }
 
   previousPage() {
     if (this.page > 1) {
       this.page = this.page - 1;
+      sessionStorage['page'] = this.page;
       this.webService.getStars(this.page).subscribe((response: any) => {
         this.stars_list = response;
       });
@@ -39,6 +47,7 @@ export class StarsComponent {
     this.webService.getLastPageNumber().subscribe((lastPageNumber: number) => {
       if (this.page < lastPageNumber) {
         this.page = this.page + 1;
+        sessionStorage['page'] = this.page;
         this.webService.getStars(this.page).subscribe((response: any) => {
           this.stars_list = response;
         });
@@ -49,5 +58,4 @@ export class StarsComponent {
   trackByName(index: number, star: any): string {
     return star.name;
   }
-  
 }

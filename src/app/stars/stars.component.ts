04a7b5c8b0aeb95c40de52764594e407010428ee
Alex McCullough <alexmccullough@outlook.com>
2024-11-28 12:03:48 +0000

    feat: star table pagination

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { WebService } from '../web.service';
+import { CommonModule, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'stars',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, DecimalPipe],
   providers: [WebService],
   templateUrl: './stars.component.html',
   styleUrl: './stars.component.css',
@@ -13,6 +13,7 @@ import { WebService } from '../web.service';
 export class StarsComponent {
   stars_list: any;
   page: number = 1;
+  lastPageNumber: number = 0;
 
   constructor(public webService: WebService) {}
 
@@ -20,6 +21,9 @@ export class StarsComponent {
     this.webService.getStars(1).subscribe((response) => {
       this.stars_list = response;
     });
+    this.webService.getLastPageNumber().subscribe((response: any) => {
+      this.lastPageNumber = response;
+    })
   }
 
   previousPage() {
@@ -31,12 +35,19 @@ export class StarsComponent {
     }
   }
 
-  // nextPage() {
-  //   if (this.page < this.webService.getLastPageNumber()) {
-  //     this.page = this.page + 1;
-  //     this.webService.getStars(this.page).subscribe((response: any) => {
-  //       this.stars_list = response;
-  //     })
-  //   }
-  // }
+  nextPage() {
+    this.webService.getLastPageNumber().subscribe((lastPageNumber: number) => {
+      if (this.page < lastPageNumber) {
+        this.page = this.page + 1;
+        this.webService.getStars(this.page).subscribe((response: any) => {
+          this.stars_list = response;
+        });
+      }
+    });
+  }
+
+  trackByName(index: number, star: any): string {
+    return star.name;
+  }
+  
 }
--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { get } from 'node:http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class WebService {
-  pageSize: number = 16;
+  pageSize: number = 12;
   pageNumber: number = 1;
 
   constructor(private http: HttpClient) {}
@@ -18,5 +19,15 @@ export class WebService {
     );
   }
 
-  getLastPageNumber() {}
+  getNumOfStars() {
+    return this.http
+      .get<any>('http://localhost:5000/api/v1.0/bodies/num_of_stars')
+      .pipe(map((response: any[]) => response[0]['Number of stars']));
+  }
+
+  getLastPageNumber() {
+    return this.getNumOfStars().pipe(
+      map((numOfStars: number) => Math.ceil(numOfStars / this.pageSize))
+    )
+  }
 }

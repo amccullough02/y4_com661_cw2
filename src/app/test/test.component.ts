bfa782bd3e77de84eafa9253518902262bdef849
Alex McCullough <alexmccullough@outlook.com>
2024-12-11 13:41:35 +0000

    test: complete suite of web service tests

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,7 +9,18 @@ import { WebService } from '../web.service';
 })
 export class TestComponent {
   test_output: string[] = [];
+  new_planet_id = '';
+
   constructor(private webService: WebService) {}
+
+  // LOGIN AND LOGOUT
+  login() {
+    this.webService
+      .login('stargal21', 'iamgamora')
+      .subscribe((response: any) => {
+        localStorage.setItem('x-access-token', response.token);
+      });
+  }
 
   // STARS AND PLANETS
 
@@ -51,10 +62,141 @@ export class TestComponent {
       });
   }
 
+  testCreatePlanet() {
+    let planet_id = '';
+    const formData = new FormData();
+    formData.append('name', 'Earth');
+    formData.append('mass', '3');
+    formData.append('radius', '6371');
+    formData.append('density', '5.51');
+    formData.append('surface_temperature', '288');
+    formData.append('apoapsis', '150000000');
+    formData.append('periapsis', '150000000');
+    formData.append('eccentricity', '0.0167');
+    formData.append('orbital_period', '365');
+    formData.append('status', 'Active');
+    formData.append('num_moons', '1');
+
+    this.webService
+      .createPlanet('67253517c5cdf6233de8bb86', formData)
+      .subscribe((response: any) => {
+        planet_id = response.url.match(/\/planets\/([^/]+)/)[1];
+        this.new_planet_id = planet_id;
+        if (
+          response.url &&
+          response.url.startsWith(
+            'http://127.0.0.1:5000/api/v1.0/bodies/67253517c5cdf6233de8bb86/planets/'
+          )
+        )
+          this.test_output.push('Create a new planet... PASS');
+        else this.test_output.push('Create a new planet... FAIL');
+
+        this.testEditPlanet(this.new_planet_id);
+      });
+  }
+
+  testEditPlanet(new_planet_id: string) {
+    const formData = new FormData();
+    formData.append('name', 'New Earth');
+    formData.append('mass', '3');
+    formData.append('radius', '6371');
+    formData.append('density', '5.51');
+    formData.append('surface_temperature', '288');
+    formData.append('apoapsis', '150000000');
+    formData.append('periapsis', '150000000');
+    formData.append('eccentricity', '0.0167');
+    formData.append('orbital_period', '365');
+    formData.append('status', 'Active');
+    formData.append('num_moons', '1');
+
+    this.webService
+      .editPlanet('67253517c5cdf6233de8bb86', new_planet_id, formData)
+      .subscribe((response: any) => {
+        if (
+          response.url &&
+          response.url.startsWith(
+            'http://127.0.0.1:5000/api/v1.0/bodies/67253517c5cdf6233de8bb86/planets/'
+          )
+        )
+          this.test_output.push('Edit a planet... PASS');
+        else this.test_output.push('Edit a planet... FAIL');
+
+        this.testDeletePlanet(this.new_planet_id);
+      });
+  }
+
+  testDeletePlanet(new_planet_id: string) {
+    this.webService
+      .deletePlanet('67253517c5cdf6233de8bb86', new_planet_id)
+      .subscribe((response: any) => {
+        if (response.message === 'planet deleted successfully')
+          this.test_output.push('Delete a planet... PASS');
+        else this.test_output.push('Delete a planet... FAIL');
+      });
+  }
+
+  testGetLogs() {
+    this.webService.getLogs().subscribe((response: any) => {
+      if (Array.isArray(response))
+        this.test_output.push('Get all logs... PASS');
+      else this.test_output.push('Get all logs... FAIL');
+    });
+  }
+
+  // USERS
+
+  testGetUsers() {
+    this.webService.getUsers().subscribe((response: any) => {
+      if (Array.isArray(response))
+        this.test_output.push('Get all users... PASS');
+      else this.test_output.push('Get all users... FAIL');
+    });
+  }
+
+  testRegister() {
+    const formData = new FormData();
+    formData.append('username', 'TestUsername');
+    formData.append('forename', 'Testforename');
+    formData.append('surname', 'Testsurname');
+    formData.append('email', 'test@email.com');
+    formData.append('password', 'averysecurepassword');
+
+    this.webService.register(formData).subscribe((response: any) => {
+      if (response.message === 'account registration successful')
+        this.test_output.push('Register new user... PASS');
+      else this.test_output.push('Register new user... FAIL');
+
+      this.testGetUser();
+    });
+  }
+
+  testGetUser() {
+    this.webService.getUser('TestUsername').subscribe((response: any) => {
+      if (response.username === 'TestUsername')
+        this.test_output.push('Fetch a user... PASS');
+      else this.test_output.push('Fetch a user... FAIL');
+
+      this.testDeleteUser();
+    });
+  }
+
+  testDeleteUser() {
+    this.webService.deleteUser('TestUsername').subscribe((response: any) => {
+      if (response.message === 'user deleted successfully')
+        this.test_output.push('Delete a user... PASS');
+      else this.test_output.push('Delete a user... FAIL');
+    });
+  }
+
   ngOnInit() {
+    this.login();
     this.testGetStars();
     this.testGetStar();
     this.testGetNumStars();
     this.testGetPlanet();
+    this.testCreatePlanet();
+    this.testGetLogs();
+    this.testGetUsers();
+    this.testRegister();
   }
 }
